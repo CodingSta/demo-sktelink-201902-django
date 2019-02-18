598b7861734c3be9ec5376587c5452312bf3d81d
Chinseok Lee <me@askcompany.kr>
2019-02-18 11:12:25 +0900

    blog.Post 모델에 대한 admin 커스텀 설정 추가

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, db_index=True)
     content = models.TextField()
     tags = models.CharField(max_length=20)
+    is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
