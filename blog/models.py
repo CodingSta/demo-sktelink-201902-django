ea494e90b20b85c95f5a4ed74c935d0e4c8e8a20
Chinseok Lee <me@askcompany.kr>
2019-02-18 11:57:04 +0900

    blog.Post/Comment 모델에 대해, 작성자(author) 필드를 추가

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,13 @@
+from django.conf import settings  # 설정값을 가져오는 올바른 방법
 from django.db import models
+# from django.contrib.auth.models import User
 
+# settings => AUTH_USER_MODEL = "auth.User"
+# settings.AUTH_USER_MODEL
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
     title = models.CharField(max_length=100, db_index=True)
     content = models.TextField()
     tags = models.CharField(max_length=20)
@@ -14,6 +20,9 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author_name = models.CharField(max_length=100)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
