705f8b5e09baa30e775e1bbd3d1489485ad2147a
Chinseok Lee <me@askcompany.kr>
2019-02-19 09:21:46 +0900

    shop.Shop/Item 모델 생성 및 추가

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Item(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    desc = models.TextField(blank=True)
+    price = models.PositiveIntegerField()
+    is_public = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
