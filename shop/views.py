14d5b4dc538d327c2ddd9cf18cba6b4e58a15aca
Chinseok Lee <me@askcompany.kr>
2019-02-21 11:06:49 +0900

    shop.shop_edit 뷰를 함수기반뷰로 구현

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Shop
 from .forms import ShopForm
 
@@ -39,3 +39,26 @@ from django.views.generic import CreateView
 shop_new_cbv = CreateView.as_view(
     model=Shop, form_class=ShopForm,
     success_url='/shop/')
+
+
+def shop_edit(request, pk):
+    # try:
+    #     shop = Shop.objects.get(pk=pk)
+    # except Shop.DoesNotExist:
+    #     raise Http404  # django.http.Http404
+
+    shop = get_object_or_404(Shop, pk=pk)
+
+    form_cls = ShopForm
+
+    if request.method == "POST":  # "GET", "POST"
+        form = form_cls(request.POST, request.FILES, instance=shop)
+        if form.is_valid():
+            shop = form.save()
+            return redirect('/shop/{}/'.format(shop.id))
+    else:
+        form = form_cls(instance=shop)
+
+    return render(request, 'shop/shop_form.html', {
+        'form': form,
+    })
