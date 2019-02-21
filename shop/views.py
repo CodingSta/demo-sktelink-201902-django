26b1a756aa5b100e5e93de4572b38cb1c8092ec6
Chinseok Lee <me@askcompany.kr>
2019-02-21 12:11:19 +0900

    shop.index/shop_detail 뷰에 대한 CBV 구현

--- shop/views.py
+++ shop/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+
 from .models import Shop
 from .forms import ShopForm
 
 
-def index(request):
-    # 전체 Shop 목록을 가져올 예정이다. (Lazy한 특성)
-    qs = Shop.objects.all()
-    return render(request, 'shop/shop_list.html', {
-        'shop_list': qs,
-    })
+# def index(request):
+#     # 전체 Shop 목록을 가져올 예정이다. (Lazy한 특성)
+#     qs = Shop.objects.all()
+#     return render(request, 'shop/shop_list.html', {
+#         'shop_list': qs,
+#     })
+
+index = ListView.as_view(model=Shop)
 
 
-def shop_detail(request, pk):
-    # 즉시 DB로부터 데이터를 가져옵니다.
-    shop = Shop.objects.get(pk=pk)
-    return render(request, 'shop/shop_detail.html', {
-        'shop': shop,
-    })
+# def shop_detail(request, pk):
+#     # 즉시 DB로부터 데이터를 가져옵니다.
+#     shop = Shop.objects.get(pk=pk)
+#     return render(request, 'shop/shop_detail.html', {
+#         'shop': shop,
+#     })
+
+shop_detail = DetailView.as_view(model=Shop)
+
 
 
 def shop_new(request):
@@ -33,8 +40,6 @@ def shop_new(request):
     return render(request, 'shop/shop_form.html', {
         'form': form,
     })
-
-from django.views.generic import CreateView
 
 shop_new_cbv = CreateView.as_view(
     model=Shop, form_class=ShopForm)
@@ -63,7 +68,6 @@ def shop_edit(request, pk):
     })
 
 
-from django.views.generic import UpdateView
 
 shop_edit_cbv = UpdateView.as_view(
     model=Shop, form_class=ShopForm)
